d9292d55ff726724c9ecc952e3f4964a791bc5e5
Carsten Führmann <cfuhrmann@gmail.com>
2021-04-26 22:10:29 +0200

    Updated all frontend packages

--- frontend/src/SolutionView.tsx
+++ frontend/src/SolutionView.tsx
@@ -1,21 +1,14 @@
-import * as React from "react";
 import ReactMarkdown from "react-markdown";
-import RemarkMathPlugin from "remark-math";
-import RemarkGfmPlugin from "remark-gfm";
-// @ts-ignore
-import { BlockMath, InlineMath } from "react-katex";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex"; // // @ts-ignore
 
 export function SolutionView(props: { solution: string }) {
   return (
     <>
       <div className="w3-container markdown-body">
         <ReactMarkdown
-          //escapeHtml={false}
-          plugins={[RemarkMathPlugin, RemarkGfmPlugin]}
-          renderers={{
-            inlineMath: ({ value }) => <InlineMath>{value}</InlineMath>,
-            math: ({ value }) => <BlockMath>{value}</BlockMath>,
-          }}
+          remarkPlugins={[remarkMath]}
+          rehypePlugins={[rehypeKatex]}
         >
           {props.solution}
         </ReactMarkdown>
