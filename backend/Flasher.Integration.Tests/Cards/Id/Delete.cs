9d8a6a439d1b4f88d5ba93680722ce08002a10b3
Carsten Führmann <cfuhrmann@gmail.com>
2025-04-20 18:13:29 +0200

    Add test about deleting card

--- backend/Flasher.Integration.Tests/Cards/Id/Delete.cs
+++ backend/Flasher.Integration.Tests/Cards/Id/Delete.cs
@@ -1,4 +1,4 @@
-﻿using System.Text;
+﻿using System.Net.Http.Json;
 using System.Text.Json;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.Configuration;
@@ -50,13 +50,7 @@ public sealed class Delete : IDisposable
         var cookies = loginResponse.GetCookies();
         client.AddCookies(cookies);
 
-        var postBodyString = $$"""
-            {
-                "prompt": "prompt",
-                "solution": "solution"
-            }
-            """;
-        var postBodyContent = new StringContent(postBodyString, Encoding.UTF8, "application/json");
+        var postBodyContent = JsonContent.Create(new { prompt = "prompt", solution = "solution" });
         using var postResponse = await client.PostAsync("/Cards", postBodyContent);
         var postResponseString = await postResponse.Content.ReadAsStringAsync();
         using var postResponseDocument = JsonDocument.Parse(postResponseString);
@@ -65,6 +59,59 @@ public sealed class Delete : IDisposable
         using var response = await client.DeleteAsync($"Cards/{postResponseId}");
 
         using var getResponse = await client.GetAsync("/Cards");
+
+        _ = await Verify(new { response, getResponse });
+    }
+
+    [Fact]
+    public async Task ReadAfterApplicationRestart()
+    {
+        var settings = new Dictionary<string, string?>
+        {
+            { "FileStore:Directory", _fileStoreDirectory },
+        };
+
+        using var factory1 = new WebApplicationFactory<Program>().WithWebHostBuilder(builder =>
+            builder.ConfigureAppConfiguration(
+                (context, conf) =>
+                {
+                    _ = conf.AddInMemoryCollection(settings);
+                }
+            )
+        );
+
+        var client = factory1.CreateClient();
+
+        using var loginResponse = await client.Login(UserName, Password);
+        var cookies = loginResponse.GetCookies();
+        client.AddCookies(cookies);
+
+        var postBodyContent = JsonContent.Create(new { prompt = "prompt", solution = "solution" });
+        using var postResponse = await client.PostAsync("/Cards", postBodyContent);
+        var postResponseString = await postResponse.Content.ReadAsStringAsync();
+        using var postResponseDocument = JsonDocument.Parse(postResponseString);
+        var postResponseId = postResponseDocument.RootElement.GetProperty("id").GetString();
+
+        using var response = await client.DeleteAsync($"Cards/{postResponseId}");
+
+        factory1.Dispose();
+
+        using var factory2 = new WebApplicationFactory<Program>().WithWebHostBuilder(builder =>
+            builder.ConfigureAppConfiguration(
+                (context, conf) =>
+                {
+                    _ = conf.AddInMemoryCollection(settings);
+                }
+            )
+        );
+
+        var clientAfterRestart = factory2.CreateClient();
+
+        using var loginResponseAfterRestart = await clientAfterRestart.Login(UserName, Password);
+        var cookiesAfterRestart = loginResponseAfterRestart.GetCookies();
+        clientAfterRestart.AddCookies(cookiesAfterRestart);
+
+        using var getResponse = await clientAfterRestart.GetAsync("/Cards");
 
         _ = await Verify(new { response, getResponse });
     }
