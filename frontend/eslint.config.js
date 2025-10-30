9a9436f916354ec85583229ffe21584df31a7540
Carsten Führmann <cfuhrmann@gmail.com>
2025-10-30 15:52:30 +0100

    Remove merge markers from eslint

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,19 +1,10 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-<<<<<<< Updated upstream
-  { ignores: ['dist'] },
-  js.configs.recommended,
-  ...tseslint.configs.recommended,
-  {
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 'latest',
-=======
   { ignores: ["dist"] },
   js.configs.recommended,
   ...tseslint.configs.recommended,
@@ -21,25 +12,18 @@ export default tseslint.config(
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: "latest",
->>>>>>> Stashed changes
       globals: globals.browser,
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
     },
     rules: {
-<<<<<<< Updated upstream
-      ...reactHooks.configs['recommended-latest'].rules,
-      'react-refresh/only-export-components': [
-        'warn',
-=======
       ...reactHooks.configs["recommended-latest"].rules,
       "react-refresh/only-export-components": [
         "warn",
->>>>>>> Stashed changes
         { allowConstantExport: true },
       ],
     },
   },
-)
+);
